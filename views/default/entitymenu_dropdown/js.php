8ccf80dfa8c99377039a400e70250547fe6d0506
Matt Beckett <beck24@gmail.com>
2013-02-14 23:33:44 -0700

    little better handling of function binding on ajax complete

--- views/default/entitymenu_dropdown/js.php
+++ views/default/entitymenu_dropdown/js.php
@@ -2,7 +2,16 @@ elgg.provide('elgg.entitymenu_dropdown');
 
 elgg.entitymenu_dropdown.init = function() {
 
-  $('.elgg-menu-item-entitymenu-dropdown').each( function(index) {
+  elgg.entitymenu_dropdown.position();
+  
+  $(document).ajaxComplete( function() {
+	elgg.entitymenu_dropdown.position();
+  });
+  
+}
+
+elgg.entitymenu_dropdown.position = function() {
+$('.elgg-menu-item-entitymenu-dropdown').each( function(index) {
 	var dropdown = $(this).children('ul.elgg-menu').eq(0);
 	var width = $(this).width();
 	var offset = dropdown.width() - width;
@@ -12,11 +21,6 @@ elgg.entitymenu_dropdown.init = function() {
 	}
 	
   });
-  
-  $(document).ajaxComplete( function() {
-	elgg.entitymenu_dropdown.init();
-  });
-  
 }
 
 elgg.register_hook_handler('init', 'system', elgg.entitymenu_dropdown.init);
